e0bd33363d6d9831326e1da15cd8d712a7cae6fe
Ennajy <younessennajy30@gmail.com>
2024-08-03 19:03:40 +0100

    end multi auth

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
+ */
+class AdminFactory extends Factory
+{
+    protected static ?string $password;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function definition(): array
+    {
+        return [
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
+        ];
+    }
+}
